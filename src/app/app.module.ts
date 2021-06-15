2c7391e15fbb5c27c81355c178d985f808ace4ef
Kristofer <kristofer.kerren@valtech.com>
2021-06-15 14:19:45 +0200

    Creating a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // All components to include
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // The root component
 })
 export class AppModule { }
