b48e33be8c819feb5cfd37ed51de49538098a571
Kristofer <kristofer.kerren@valtech.com>
2021-06-16 20:30:25 +0200

    Assignment 1: Working with components (warning and success messages)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,7 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]', //attribute selector
   selector: '.app-servers', // class selector
   //either template or templateUrl needs to be in @Component
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: '/servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
